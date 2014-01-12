a8f1a5f5d94afa73e0eca2f620889c40def9a17f
Thomas Klemz <tjklemz@gmail.com>
2014-01-11 22:25:49 -0800

    initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clojure-game "your.project.version"
+  :description "A description of your project"
+  :main ^:skip-aot clojure-game.main
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [penumbra "0.6.0"]])
+
